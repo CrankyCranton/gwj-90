ff6ec896fe799c8c7a1b729e0e26701aef9423c7
CrankyCranton <crantonbogsmorehooversil@gmail.com>
2026-02-16 00:11:38 -0500

    Signals bus template (not connected or emitted yet)

--- game/core/autoloads/auto_signal_bus.gd
+++ game/core/autoloads/auto_signal_bus.gd
@@ -1,1 +1,23 @@
 extends Node
+
+
+signal card_gained(index: int, type: Location.Type)
+signal card_burnt(index: int)
+signal use_card(index: int)
+
+signal move_character(character: Character, location: Location)
+signal fetch_valid_character_movement(character: Character, valid_location_types: Array[Location.Type])
+signal return_valid_character_movement(character: Character, locations: Array[Location])
+
+signal character_selected(character: Character)
+signal character_deselected(character: Character)
+signal location_selected(location: Location)
+signal location_deselected(location: Location)
+# By default, all locations are disabled.
+signal enable_location(location: Location)
+signal disable_location(location: Location)
+
+signal loop_scored(loop_score: int)
+signal score_changed(score: int)
+signal won
+signal lost
