69a851d0b4c5f34369a114d42048cb1db8b747e0
CrankyCranton <crantonbogsmorehooversil@gmail.com>
2026-02-20 11:38:30 -0500

    Merchant

--- character/player_character/merchant/merchant.gd
+++ character/player_character/merchant/merchant.gd
@@ -1,1 +1,6 @@
 class_name Merchant extends PlayerCharacter
+
+
+func _update_character_score() -> void:
+	@warning_ignore("integer_division")
+	points = floori(paths.back().locations.size() / 5) * 3
