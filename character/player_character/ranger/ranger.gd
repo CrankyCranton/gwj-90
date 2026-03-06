db4ac30ae4a33cd9cc566960ca7d3853950573da
CrankyCranton <crantonbogsmorehooversil@gmail.com>
2026-03-06 15:48:46 -0500

    Fix ranger increasing location view range

--- character/player_character/ranger/ranger.gd
+++ character/player_character/ranger/ranger.gd
@@ -7,3 +7,4 @@ class_name Ranger extends PlayerCharacter
 func set_location(new_location: Location) -> void:
 	new_location.view_range += view_bonus
 	await super(new_location)
+	new_location.view_range -= view_bonus
